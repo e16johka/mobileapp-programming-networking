a2ff6c08ec1baab24767cbeb1357cc5067c13e20
e16johka <e16johka@student.his.se>
2022-05-11 16:02:36 +0200

    Fick hjälp att städa upp röran jag skapat

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,24 +4,14 @@ import android.os.Bundle;
 import android.util.Log;
 import java.util.ArrayList;
 
-ArrayList<String> mountains = new ArrayList<String>(); // Create an ArrayList object
 
-    public class Main {
-    public static void main(String[] args) {
-        ArrayList<String> mountains = new ArrayList<String>();
-        mountains.add("Kebnekaise");
-        mountains.add("Mount Everest");
-        mountains.add("Danmarks hogsta kulle");
-        mountains.add("Kilimanjaro");
-        System.out.println(mountains);
-    }
-}
 
 import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
+    ArrayList<String> mountains = new ArrayList<String>(); // Create an ArrayList object
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
